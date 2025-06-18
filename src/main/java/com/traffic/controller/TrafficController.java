e7a2f57e52e27433553393b07a24162198e04f25
NavyaReddi <navyareddi04@gmail.com>
2025-06-18 12:15:32 +0530

    Updated congestion prediction logic

--- src/main/java/com/traffic/controller/TrafficController.java
+++ src/main/java/com/traffic/controller/TrafficController.java
@@ -1,5 +1,4 @@
 package com.traffic.controller;
-package com.traffic.controller;
 
 import org.springframework.web.bind.annotation.*;
 
@@ -9,13 +8,8 @@ public class TrafficController {
 
     @GetMapping("/predict")
     public String predict(@RequestParam int hour, @RequestParam int vehicles) {
-        if (vehicles > 1000 && (hour >= 17 && hour <= 20)) {
+        if (vehicles > 1000 || (hour >= 17 && hour <= 20))
             return "Prediction: Heavy Congestion";
-        } else if (vehicles > 500) {
-            return "Prediction: Moderate Congestion";
-        } else {
-            return "Prediction: Low Congestion";
-        }
+        return "Prediction: Moderate/Low Congestion";
     }
 }
-
