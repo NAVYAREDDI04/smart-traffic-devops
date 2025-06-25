51c2fd5ec02e1f298dbb955e0b6e43a424e9a82e
NavyaReddi <navyareddi04@gmail.com>
2025-06-25 09:50:01 +0530

    Updated pom.xml

---
+++ traffic-api/src/main/java/com/traffic/TrafficAppApplication.java
@@ -0,0 +1,12 @@
+package com.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficApiApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Smart traffic app running.....");
+    }
+}
